a7817dabb1aef33609efd5467c6e8571eb66dedb
Fiusion <stefano.poropat@skole.hr>
2024-03-04 20:29:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/eclipse/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.eclipse;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.menu);
+        TextView txt2 = findViewById(R.id.txt2);
+        Date c = Calendar.getInstance().getTime();
+        System.out.println("Current time => " + c);
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+        String formattedDate = df.format(c);
+        txt2.setText("The last total Solar eclipse was on " + formattedDate);
+    }
+
+    /*public void Calc(View view) {
+        //Update will be added later
+    }*/
+}
