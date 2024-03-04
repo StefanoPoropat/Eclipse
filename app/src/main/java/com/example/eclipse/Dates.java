8912ccc9ac906e515041e1d565e2876695988911
Fiusion <stefano.poropat@skole.hr>
2024-03-04 20:29:43 +0100

    Added new libs

---
+++ app/src/main/java/com/example/eclipse/Dates.java
@@ -0,0 +1,6 @@
+package com.example.eclipse;
+
+public class Dates {
+    public static String[] future = {"08 April 2024", "18 April 2024", "28 April 2024"};
+    public static String[] past = {};
+}
